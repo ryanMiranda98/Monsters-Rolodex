b4ee041b66323c39c1ec8ac1140ee16ea7252fd5
unknown <ryanjude.miranda@gmail.com>
2020-07-11 18:46:46 +0530

    Updates

---
+++ precache-manifest.5ca047574cd612c45f8b5f025fceabb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be01c6a2990cb9db15b4e94d358ddd5",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "04adb55212a732f00586",
+    "url": "/Monsters-Rolodex/static/css/main.45121a1e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04adb55212a732f00586",
+    "url": "/Monsters-Rolodex/static/js/main.01974bd0.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
